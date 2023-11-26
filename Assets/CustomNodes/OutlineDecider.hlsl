57c1a60f4c4f3e4bb7ecc30af8612c70e580f5fb
ledsna <45171833+ledsna@users.noreply.github.com>
2023-11-26 21:55:24 +0300

    final commit? ? ?? ? ?

--- Assets/CustomNodes/OutlineDecider.hlsl
+++ Assets/CustomNodes/OutlineDecider.hlsl
@@ -2,11 +2,12 @@
 #define OUTLINE_DECIDER_INCLUDED
 
 void OutlineDecider_float(float NormalEdge, float DepthEdge, float DepthDifference,
+    float Rs, float Gs, float Bs, 
     out float R, out float G, out float B, out float A) 
 {
-    R = 0;
-    G = 0;
-    B = 0;
+    R = Rs;
+    G = Gs;
+    B = Bs;
     if (DepthDifference < 0)
     {
         A = 0;
@@ -15,13 +16,16 @@ void OutlineDecider_float(float NormalEdge, float DepthEdge, float DepthDifferen
     {
         if (DepthEdge > 0.0)
         {
+            R *= 0.5;
+            G *= 0.5;
+            B *= 0.5;
             A = DepthEdge;
         }
         else
         {
-            R = 1;
-            G = 1;
-            B = 1;
+            R *= 1.5;
+            G *= 1.5;
+            B *= 1.5;
             A = NormalEdge;
         }
     }
