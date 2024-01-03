1777cc8bf240f13dd4e4dff0bf2683b0d0b267ff
Dmitry Aspisov <56952323+aspisov@users.noreply.github.com>
2024-01-03 20:54:40 +0300

    blocky movemet

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -6,12 +6,17 @@ public class CameraManager : MonoBehaviour
 {
     public static CameraManager instance;
 
-    public Transform player;
+    [SerializeField] Camera mainCamera;
+    [SerializeField] Transform player;
 
     [Header("Camera Settings")]
     [SerializeField] float cameraSmoothSpeed = 7; // the bigger this number is the longer it takes your camera to catch up
     private Vector3 cameraVelocity;
+
+    [Header("Camera Free Rotation Settings")]
     [SerializeField] float cameraLookSpeed = 10;
+    private bool cameraIsMoving = false;
+
 
     private void Awake()
     {
@@ -30,16 +35,13 @@ public class CameraManager : MonoBehaviour
         if (PlayerInputManager.instance.cameraMovementInput != Vector2.zero)
         {
             Vector3 movementDirection = transform.forward * PlayerInputManager.instance.cameraMovementInput.y +
-                transform.right * PlayerInputManager.instance.cameraMovementInput.x;
+                                        transform.right * PlayerInputManager.instance.cameraMovementInput.x;
             movementDirection.Normalize();
             movementDirection.y = 0;
 
             transform.position += movementDirection * cameraLookSpeed * Time.deltaTime;
-
-            return;
         }
-
-        if (player != null)
+        else if (player is not null)
         {
             Vector3 targetCameraPosition = Vector3.SmoothDamp
             (transform.position,
@@ -49,5 +51,23 @@ public class CameraManager : MonoBehaviour
 
             transform.position = targetCameraPosition;
         }
+        
+        AdjustCameraPosition();
+    }
+
+    private void AdjustCameraPosition()
+    {
+        float pixelsPerUnit = mainCamera.scaledPixelHeight / mainCamera.orthographicSize / 2;
+        
+        float newPosX = Mathf.Round(transform.position.x * pixelsPerUnit) / pixelsPerUnit;
+        float xDifference = newPosX - transform.position.x;
+        
+        float newPosY = Mathf.Round(transform.position.y * pixelsPerUnit) / pixelsPerUnit;
+        float yDifferece = newPosY - transform.position.y; 
+            
+        float newPosZ = Mathf.Round(transform.position.z * pixelsPerUnit) / pixelsPerUnit;
+        float zDifference = newPosZ - transform.position.z;
+
+        transform.position = new Vector3(newPosX, newPosY, newPosZ);
     }
 }
